8948e91bb5cf7bd9c802a852918942f2c18ab7ab
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2023-01-21 10:45:52 -0600

    Configure string SQL and inject depedency from Web Application

---
+++ SalesSystem.IOC/Dependency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesSystem.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+/*using SalesSystem.DAL.Interfaces;
+using SalesSystem.DAL.Implementation;
+using SalesSystem.BLL.Interfaces;
+using SalesSystem.BLL.Implementation;*/
+
+namespace SalesSystem.IOC
+{
+    public static class Dependency
+    {
+        public static void InjectDependency(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<salessystemContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("StringSQL"));
+            });
+        }
+    }
+}
--- SalesSystem.WebApplication/Program.cs
+++ SalesSystem.WebApplication/Program.cs
@@ -1,7 +1,10 @@
+using SalesSystem.IOC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.InjectDependency(builder.Configuration);
 
 var app = builder.Build();
 
