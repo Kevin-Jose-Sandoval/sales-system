f698a125e48391e8411864cd37d073f2ae1d18ef
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2023-01-21 20:51:30 -0600

    Integrate templates to project

---
+++ SalesSystem.WebApplication/Controllers/SaleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesSystem.WebApplication.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult NewSale()
+        {
+            return View();
+        }
+
+        public IActionResult SaleHistory()
+        {
+            return View();
+        }
+    }
+}
